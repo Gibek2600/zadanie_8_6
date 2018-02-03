4a5152c260d8a7c17992a73dde915162370eba05
Michał Gibczyński <Gibek2600@interia.pl>
2018-02-03 19:51:25 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var selectedTab = 'details';
+
+if (selectedTab == 'details') {
+    console.log('You selected details tab');
+} else if (selectedTab == 'comments') {
+    console.log('You selected comments tab');
+} else {
+    console.log('You selected product description tab');
+}
